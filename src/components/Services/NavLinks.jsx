67d411b8d407d77b127f2ff5b9a3d20894fe44c6
Venktesh Soma <venkteshs@cybage.com>
2022-04-14 18:05:58 +0530

    Reusable component for Navbar navlinks

---
+++ src/components/Services/NavLinks.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function NavLinks() {
+  return (
+    <React.Fragment>
+      <div>
+        <li>Home</li>
+      </div>
+
+      <div>
+        <li>About</li>
+      </div>
+
+      <div>
+        <li>Projects</li>
+      </div>
+
+      <div>
+        <li>Resume</li>
+      </div>
+
+      <div>
+        <li>Contact</li>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default NavLinks;
