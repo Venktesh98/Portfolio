21605881c59d6bba989353d1a2b3e845e3dbbaf0
Venktesh Soma <venkteshs@cybage.com>
2022-05-26 14:19:47 +0530

    Implemented the resume section with parallex!

---
+++ src/components/Portfolio/Resume/Resume.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useStyles } from "./Resume.style";
+
+function Resume() {
+  const classes = useStyles();
+  return (
+    <section className={classes.resumeSection} id="resume">
+      <div className={classes.parallex}></div>
+      <div className={classes.resumeHeaderLineAndResumeCopy}>
+        Check Out My Resume!
+      </div>
+      <div
+        className={`${classes.resumeHeaderLineAndResumeCopy} ${classes.resumeCopy}`}
+      >
+        <a
+          href="https://drive.google.com/file/d/1JpZPm3MwzeIUe5d5V-jBHRhl6BgUi7IF/view?usp=sharing"
+          download
+          className={classes.resumeCopyLine}
+        >
+          Get a copy
+        </a>
+        {/* <a
+          href={process.env.PUBLIC_URL + "/assets/contact.jpg"}
+          download
+          className={classes.resumeCopyLine}
+        >
+          Get a copy
+        </a> */}
+      </div>
+    </section>
+  );
+}
+
+export default Resume;
---
+++ src/components/Portfolio/Resume/Resume.style.jsx
@@ -0,0 +1,57 @@
+import { makeStyles } from "@material-ui/core";
+
+export const useStyles = makeStyles((theme) => ({
+  resumeSection: {
+    position: "relative",
+    // border: "2px solid yellow",
+    marginTop: "5%",
+  },
+  parallex: {
+    // backgroundImage: `linear-gradient(rgba(20, 32, 46, 0.2), rgb(31 41 55 / 70%)),url(${
+    //   process.env.PUBLIC_URL + "/assets/laptop.jpg"
+    // })`,
+    backgroundImage: `linear-gradient(rgba(20, 32, 46, 0.2), rgb(31 41 55 / 70%)),url(${
+      process.env.PUBLIC_URL + "/assets/laptopPen.jpg"
+    })`,
+    minHeight: "350px",
+    backgroundAttachment: "fixed",
+    backgroundPosition: "bottom",
+    // backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    margin: "0",
+  },
+  resumeHeaderLineAndResumeCopy: {
+    position: "absolute",
+    top: "40%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    fontSize: "45px",
+    color: "white",
+    fontWeight: "200",
+    textTransform: "uppercase",
+    letterSpacing: "5px",
+    width: "100%",
+    textAlign: "center",
+  },
+  resumeCopy: {
+    marginTop: "8%",
+    fontSize: "18px",
+    border: "1px solid white",
+    width: "13.5%",
+    height: "60px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: "6px",
+
+    "&:hover": {
+      border: "1px solid white",
+      backgroundColor: "rgba(20, 32, 46, 0.5)",
+      cursor: "pointer",
+    },
+  },
+  resumeCopyLine: {
+    textDecoration: "none",
+    color: "white",
+  },
+}));
