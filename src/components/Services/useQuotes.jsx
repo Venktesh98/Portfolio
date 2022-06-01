5605b36099d1293656f8ea0e7daed047e6abcc12
Venktesh Soma <venkteshs@cybage.com>
2022-06-01 13:16:01 +0530

    Created the Quotes List

---
+++ src/components/Services/useQuotes.jsx
@@ -0,0 +1,93 @@
+export const useQuotes = () => {
+    const QuotesList = [
+        {
+            quote: "Too many of us are not living our dreams because we are living our fears.",
+            author: "Les Brown"
+        },
+        {
+            quote: "Whatever the mind of man can conceive and believe, it can achieve.",
+            author: "Napoleon Hill"
+        },
+        {
+            quote: "From error to error one discovers the entire truth.",
+            author: "Sigmund Freud"
+        },
+        {
+            quote: "You can observe a lot just by watching.",
+            author: "Yogi Berra"
+        },
+        {
+            quote: "The two most powerful warriors are patience and time.",
+            author: "Leo Tolstoy"
+        },
+        {
+            quote: "A coder is a poet.",
+            author: "Venktesh Soma"
+        },
+        {
+            quote: "If at first you don't succeed, destroy all evidence that you tried.",
+            author: "Steven Wright"
+        },
+        {
+            quote: "All you need in this life is ignorance and confidence; then success is true.",
+            author: "Mark Twain"
+        },
+        {
+            quote: "Winners never Quit, Quitters never Win.",
+            author: "Napoleon Hill"
+        },
+        {
+            quote: "Try not to become a man of success. Rather become a man of value.",
+            author: "Albert Einstein"
+        },
+        {
+            quote: "The only guarantee for failure is stop trying.",
+            author: "John C Maxwell"
+        },
+        {
+            quote: "Vision without execution is just hallucination.",
+            author: "Henry Ford"
+        },
+        {
+            quote: "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+            author: "Mahatma Ghandhi"
+        },
+        {
+            quote: "You have to dream before your dream can come true.",
+            author: "A.P.J. Abdul Kalam"
+        },
+        {
+            quote: "We all need people who will give us feedback.That's how we improve.",
+            author: "Bill Gates"
+        },
+        {
+            quote: "Your time is limited, so don't waste it living someone else's life.",
+            author: "Steve Jobs"
+        },
+        {
+            quote: "The best revenge is massive success.",
+            author: "Frank Sinatra"
+        },
+        {
+            quote: "It's fine to celebrate success but it is more important to heed the lessons of failure.",
+            author: "Bill Gates"
+        },
+        {
+            quote: "Success usually comes to those who are too busy to be looking for it.",
+            author: "Henry David Thoreau"
+        },
+        {
+            quote: "Stop chasing the money and start chasing the passion.",
+            author: "Tony Hsieh"
+        },
+        {
+            quote: " Don’t see others doing better than you, beat your own records every day because success is a fight between you and yourself.",
+            author: "Chandrasekhar Azad"
+        },
+        {
+            quote: " Don’t take rest after your first victory because if you fail in second, more lips are waiting to say that your first victory was just luck.",
+            author: "A. P. J. Abdul Kalam"
+        },
+    ]
+    return QuotesList
+}
