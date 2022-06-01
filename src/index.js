d1e6359b60a3618c107fb9f43d64aeca8eb22823
Venktesh Soma <venkteshs@cybage.com>
2022-06-01 13:18:17 +0530

    Replaced the App.js to Backdrop component that will be loaded on the initial render of the website

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import BackDropComponent from "./components/Portfolio/Backdrop/BackDropComponent";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BackDropComponent />
   </React.StrictMode>,
   document.getElementById("root")
 );
