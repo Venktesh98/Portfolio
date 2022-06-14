31ad76ab5687589a81f855db0d335c4d90aeae3b
Venktesh Soma <venkteshs@cybage.com>
2022-06-14 17:46:43 +0530

    Optimised the code in Project component and opened the github url when clicked on card

--- src/components/Portfolio/Projects/Projects.jsx
+++ src/components/Portfolio/Projects/Projects.jsx
@@ -12,6 +12,50 @@ function Projects() {
   const classes = useStyles();
   const projectLists = useProjectLists();
 
+  const displayProjectCards = (projectItem) => {
+    return (
+      <Card className={classes.projectCard} key={projectItem.id}>
+        <CardActionArea>
+          <a
+            href={projectItem.projectLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <CardMedia
+              className={classes.media}
+              image={projectItem.projectImage}
+              title={projectItem.projectName}
+            />
+          </a>
+          <CardContent className={classes.cardContent}>
+            <div className={classes.projectTitle}>
+              {projectItem.projectName}
+            </div>
+            <div className={classes.backupOptionsContainer}>
+              <a
+                href={projectItem.projectLink}
+                className={classes.backupOptionsLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className={classes.backupOptionsText}>
+                  {projectItem.isProjectInGitlab ? "Gitlab" : "Github"}
+                </span>
+                <>
+                  {projectItem.isProjectInGitlab ? (
+                    <SiGitlab className={classes.backupIcon} />
+                  ) : (
+                    <SiGithub className={classes.backupIcon} />
+                  )}
+                </>
+              </a>
+            </div>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    );
+  };
+
   return (
     <section id="projects">
       <div className={classes.projects}>Projects</div>
@@ -20,29 +64,8 @@ function Projects() {
       </div>
 
       <div className={classes.projectsWrapper}>
-        {projectLists.map((projectItem) => (
-          <Card className={classes.projectCard} key={projectItem.id}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image={projectItem.projectImage}
-                title={projectItem.projectName}
-              />
-              <CardContent className={classes.cardContent}>
-                <div className={classes.projectTitle}>{projectItem.projectName}</div>
-                <div className={classes.backupOptionsContainer}>
-                  <a href={projectItem.projectLink} className={classes.backupOptionsLink} target="_blank"
-                    rel="noopener noreferrer">
-                    <span className={classes.backupOptionsText}>{projectItem.isProjectInGitlab ? "Gitlab" : "Github"}</span> <> {projectItem.isProjectInGitlab ? <SiGitlab className={classes.backupIcon} /> : <SiGithub className={classes.backupIcon} />}</>
-                  </a>
-                </div>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        ))}
-
+        {projectLists.map((projectItem) => displayProjectCards(projectItem))}
       </div>
-
     </section>
   );
 }
